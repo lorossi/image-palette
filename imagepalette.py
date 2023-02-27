a36a31decc72b5725c22c89166f3256938fe53d5
lorossi <mail@lorenzoros.si>
2023-02-27 23:14:43 +0100

    fixed commands

--- imagepalette.py
+++ imagepalette.py
@@ -3,6 +3,7 @@
 import argparse
 import logging
 
+from modules.color import Color
 from modules.palette_extractor import PaletteExtractor
 from modules.position import Position
 
@@ -29,7 +30,7 @@ def main():
         "--palette", help="Create an image containing the palette", action="store_true"
     )
     parser.add_argument(
-        "--print-palette", help="Print the palette in the console", action="store_true"
+        "--print", help="Print the palette in the console", action="store_true"
     )
     parser.add_argument(
         "--json", help="Create a JSON file containing the palette", action="store_true"
@@ -47,19 +48,20 @@ def main():
         default=0.9,
     )
     parser.add_argument(
-        "--palette-width",
-        help="Ratio of the palette to the original image "
-        "(valid if used in the incorporated mode). "
-        "Default: 0.02",
+        "--color-width-scl",
+        help="Width of the color rectangle relative to the size of the palette "
+        "(valid if used in the incorporated mode)."
+        "Default: 0.75",
         type=float,
-        default=0.015,
+        default=0.75,
     )
     parser.add_argument(
-        "--palette-height",
-        help="Ratio of the palette to the original image "
-        "(valid if used in the incorporated mode). Default: 0.9",
+        "--color-height-scl",
+        help="Height of the color rectangle relative to the size of the palette "
+        "(valid if used in the incorporated mode)."
+        "Default: 0.75",
         type=float,
-        default=0.9,
+        default=0.75,
     )
     parser.add_argument(
         "--position",
@@ -81,11 +83,10 @@ def main():
     parser.add_argument(
         "--outline",
         help="Outline color of the palette. Pass 3 integers in range 0-255 "
-        " (valid if used in the incorporated mode). Example: 244 34 111. "
-        "Default 127 127 127",
+        " (valid if used in the incorporated mode). Example: 244 34 111. ",
         nargs="+",
         type=int,
-        default=[127, 127, 127],
+        default=None,
     )
     parser.add_argument(
         "--outline-width",
@@ -93,12 +94,6 @@ def main():
         "(valid if used in the incorporated mode). Default 1",
         type=int,
         default=1,
-    )
-    parser.add_argument(
-        "--no-outline",
-        help="Removes the outline of the palette "
-        "(valid if used in the incorporated mode).",
-        action="store_true",
     )
     parser.add_argument(
         "--seed",
@@ -113,6 +108,14 @@ def main():
         type=int,
         default=35,
     )
+    parser.add_argument(
+        "--max-iterations",
+        help="Maximum number of iterations for the KMeans algorithm without changes "
+        "in the objective function."
+        "Default: 10",
+        type=int,
+        default=10,
+    )
 
     args = parser.parse_args()
 
@@ -120,11 +123,13 @@ def main():
     if not args.input:
         parser.error("Specify the input image. Use -h to get a list of commands.")
 
-    if (
-        not args.print_palette
-        and not args.palette
-        and not args.json
-        and not args.incorporated
+    if not any(
+        [
+            args.palette,
+            args.print,
+            args.json,
+            args.incorporated,
+        ]
     ):
         parser.error(
             "Specify the type of output (Palette, Printpalette, Incorporated, JSON). "
@@ -137,7 +142,9 @@ def main():
             "Use -h to get a list of commands."
         )
 
-    if len(args.outline) > 3 or max(args.outline) > 255 or min(args.outline) < 0:
+    if args.outline and (
+        len(args.outline) > 3 or max(args.outline) > 255 or min(args.outline) < 0
+    ):
         parser.error(
             "The outline specified is wrong. Use -h to get a list of commands."
         )
@@ -171,9 +178,11 @@ def main():
     # fire up the extractor and load an image
     p = PaletteExtractor()
     p.loadImage(path=args.input, palette_size=args.colors, resize=args.resize)
-    p.extractColors(seed=args.seed, min_dist=args.min_color_distance)
+    p.extractColors(
+        seed=args.seed, min_dist=args.min_color_distance, max_iter=args.max_iterations
+    )
 
-    if args.print_palette:
+    if args.print:
         p.printPalette()
     if args.palette:
         p.generatePalette()
@@ -181,15 +190,17 @@ def main():
     if args.json:
         p.savePaletteJSON()
     if args.incorporated:
-        background_color = tuple(args.color)
-        if not args.no_outline:
-            outline_color = tuple(args.outline)
+        background_color = Color(*args.color)
+
+        if args.outline is not None:
+            outline_color = Color(*args.outline)
         else:
             outline_color = None
+
         p.incorporatePalette(
             output_scl=args.scl,
-            palette_width_scl=args.palette_width,
-            palette_height_scl=args.palette_height,
+            color_width_scl=args.color_width_scl,
+            color_height_scl=args.color_height_scl,
             position=Position(args.position),
             background_color=background_color,
             outline_color=outline_color,
